d7aef0bf76ed379cbee5e58e930a463b5c5cf327
Yuki Suga <ysuga@ysuga.net>
2014-10-22 18:37:10 +0900

    first commit

---
+++ include/IMURazor9DOF/IMURazor9DOF.h
@@ -0,0 +1,348 @@
+// -*- C++ -*-
+/*!
+ * @file  IMURazor9DOF.h
+ * @brief 9DOF Razor IMU (sparfun) RT component
+ * @date  $Date$
+ *
+ * @copyright Ogata Laboratory, Waseda University, 2014
+ */
+
+#ifndef IMURAZOR9DOF_H
+#define IMURAZOR9DOF_H
+
+#include <rtm/Manager.h>
+#include <rtm/DataFlowComponentBase.h>
+#include <rtm/CorbaPort.h>
+#include <rtm/DataInPort.h>
+#include <rtm/DataOutPort.h>
+#include <rtm/idl/BasicDataTypeSkel.h>
+#include <rtm/idl/ExtendedDataTypesSkel.h>
+#include <rtm/idl/InterfaceDataTypesSkel.h>
+
+// Service implementation headers
+// <rtc-template block="service_impl_h">
+
+// </rtc-template>
+
+// Service Consumer stub headers
+// <rtc-template block="consumer_stub_h">
+
+// </rtc-template>
+
+#include "SerialPort.h"
+using namespace RTC;
+
+class WorkerThread;
+
+/*!
+ * @class IMURazor9DOF
+ * @brief 9DOF Razor IMU (sparfun) RT component
+ *
+ */
+class IMURazor9DOF
+  : public RTC::DataFlowComponentBase
+{
+ public:
+  /*!
+   * @brief constructor
+   * @param manager Maneger Object
+   */
+  IMURazor9DOF(RTC::Manager* manager);
+
+  /*!
+   * @brief destructor
+   */
+  ~IMURazor9DOF();
+
+  // <rtc-template block="public_attribute">
+  
+  // </rtc-template>
+
+  // <rtc-template block="public_operation">
+  
+  // </rtc-template>
+
+  /***
+   *
+   * The initialize action (on CREATED->ALIVE transition)
+   * formaer rtc_init_entry() 
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onInitialize();
+
+  /***
+   *
+   * The finalize action (on ALIVE->END transition)
+   * formaer rtc_exiting_entry()
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onFinalize();
+
+  /***
+   *
+   * The startup action when ExecutionContext startup
+   * former rtc_starting_entry()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onStartup(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The shutdown action when ExecutionContext stop
+   * former rtc_stopping_entry()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onShutdown(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The activated action (Active state entry action)
+   * former rtc_active_entry()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onActivated(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The deactivated action (Active state exit action)
+   * former rtc_active_exit()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onDeactivated(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The execution action that is invoked periodically
+   * former rtc_active_do()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+   virtual RTC::ReturnCode_t onExecute(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The aborting action when main logic error occurred.
+   * former rtc_aborting_entry()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onAborting(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The error action in ERROR state
+   * former rtc_error_do()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onError(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The reset action that is invoked resetting
+   * This is same but different the former rtc_init_entry()
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onReset(RTC::UniqueId ec_id);
+  
+  /***
+   *
+   * The state update action that is invoked after onExecute() action
+   * no corresponding operation exists in OpenRTm-aist-0.2.0
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onStateUpdate(RTC::UniqueId ec_id);
+
+  /***
+   *
+   * The action that is invoked when execution context's rate is changed
+   * no corresponding operation exists in OpenRTm-aist-0.2.0
+   *
+   * @param ec_id target ExecutionContext Id
+   *
+   * @return RTC::ReturnCode_t
+   * 
+   * 
+   */
+  // virtual RTC::ReturnCode_t onRateChanged(RTC::UniqueId ec_id);
+
+
+ protected:
+  // <rtc-template block="protected_attribute">
+  
+  // </rtc-template>
+
+  // <rtc-template block="protected_operation">
+  
+  // </rtc-template>
+
+  // Configuration variable declaration
+  // <rtc-template block="config_declare">
+  /*!
+   * 
+   * - Name:  debug
+   * - DefaultValue: 0
+   */
+  int m_debug;
+  /*!
+   * 
+   * - Name:  port
+   * - DefaultValue: COM2
+   */
+  std::string m_port;
+  /*!
+   * 
+   * - Name:  baudrate
+   * - DefaultValue: 57600
+   */
+  int m_baudrate;
+
+  // </rtc-template>
+
+  // DataInPort declaration
+  // <rtc-template block="inport_declare">
+  
+  // </rtc-template>
+
+
+  // DataOutPort declaration
+  // <rtc-template block="outport_declare">
+  RTC::TimedAcceleration3D m_accel;
+  /*!
+   */
+  OutPort<RTC::TimedAcceleration3D> m_accelOut;
+  RTC::TimedAngularVelocity3D m_angularVel;
+  /*!
+   */
+  OutPort<RTC::TimedAngularVelocity3D> m_angularVelOut;
+  RTC::TimedDoubleSeq m_magnet;
+  /*!
+   */
+  OutPort<RTC::TimedDoubleSeq> m_magnetOut;
+  
+  // </rtc-template>
+
+  // CORBA Port declaration
+  // <rtc-template block="corbaport_declare">
+  
+  // </rtc-template>
+
+  // Service declaration
+  // <rtc-template block="service_declare">
+  
+  // </rtc-template>
+
+  // Consumer declaration
+  // <rtc-template block="consumer_declare">
+  
+  // </rtc-template>
+
+ private:
+  // <rtc-template block="private_attribute">
+  
+  // </rtc-template>
+
+  // <rtc-template block="private_operation">
+  
+  // </rtc-template>
+
+	 ssr::TimeSpec m_timestamp;
+	 ssr::Timer m_timer;
+
+	ssr::SerialPort *m_pSerialPort;
+
+	public:
+
+	RTC::ReturnCode_t onProcess();
+
+	
+
+	WorkerThread* m_pThread;
+};
+
+#include "Thread.h"
+
+
+class WorkerThread : public ssr::Thread {
+
+public:
+	WorkerThread(IMURazor9DOF* pRTC) {
+		m_pRTC = pRTC;
+	}
+
+	~WorkerThread() {}
+
+public:
+
+	void Run() {
+		endflag = false;
+		while(!endflag) {
+			m_pRTC->onProcess();
+		}
+	}
+
+	void stop() {
+		endflag = true;
+		Join();
+	}
+
+	bool endflag;
+	IMURazor9DOF *m_pRTC;
+};
+
+
+
+extern "C"
+{
+  DLL_EXPORT void IMURazor9DOFInit(RTC::Manager* manager);
+};
+
+#endif // IMURAZOR9DOF_H
---
+++ src/IMURazor9DOF.cpp
@@ -0,0 +1,313 @@
+// -*- C++ -*-
+/*!
+ * @file  IMURazor9DOF.cpp
+ * @brief 9DOF Razor IMU (sparfun) RT component
+ * @date $Date$
+ *
+ * @copyright Ogata Laboratory, Waseda University, 2014
+ */
+
+#include "IMURazor9DOF.h"
+#include "TimeSpec.h"
+#include "Timer.h"
+#include "Thread.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* imurazor9dof_spec[] =
+  {
+    "implementation_id", "IMURazor9DOF",
+    "type_name",         "IMURazor9DOF",
+    "description",       "9DOF Razor IMU (sparfun) RT component",
+    "version",           "1.0.0",
+    "vendor",            "Ogata Lab",
+    "category",          "Experimental",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    // Configuration variables
+    "conf.default.debug", "0",
+    "conf.default.port", "\\\\.\\COM20",
+    "conf.default.baudrate", "57600",
+    // Widget
+    "conf.__widget__.debug", "text",
+    "conf.__widget__.port", "text",
+    "conf.__widget__.baudrate", "text",
+    // Constraints
+
+	"exec_cxt.periodic.rate", "100000",
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+IMURazor9DOF::IMURazor9DOF(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_accelOut("accel", m_accel),
+    m_angularVelOut("angularVel", m_angularVel),
+    m_magnetOut("magnet", m_magnet)
+
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+IMURazor9DOF::~IMURazor9DOF()
+{
+}
+
+
+
+RTC::ReturnCode_t IMURazor9DOF::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  
+  // Set OutPort buffer
+  addOutPort("accel", m_accelOut);
+  addOutPort("angularVel", m_angularVelOut);
+  addOutPort("magnet", m_magnetOut);
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // Bind variables and configuration variable
+  bindParameter("debug", m_debug, "0");
+  bindParameter("port", m_port, "COM2");
+  bindParameter("baudrate", m_baudrate, "57600");
+  // </rtc-template>
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t IMURazor9DOF::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t IMURazor9DOF::onStartup(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t IMURazor9DOF::onShutdown(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+RTC::ReturnCode_t IMURazor9DOF::onActivated(RTC::UniqueId ec_id)
+{
+	std::cout << "[RTC.IMURazor9DOF] onActivated" << std::endl;
+	std::cout << "[RTC.IMURazor9DOF] Opening " << m_port << std::endl;
+	try {
+		m_pSerialPort = new ssr::SerialPort(m_port.c_str(), m_baudrate);
+	} catch (ssr::ComException& ex) {
+		std::cout << "[RTC.IMURazor9DOF] Opening " << m_port << " failed." << std::endl;
+		std::cout << "[RTC.IMURazor9DOF] Exception: " << ex.what() << std::endl;
+		m_pSerialPort = NULL;
+		return RTC::RTC_ERROR;
+	}
+
+	m_pSerialPort->FlushRxBuffer();
+
+	ssr::Timer timer;
+	timer.tick();
+	
+	std::cout << "[RTC.IMURazor9DOF] Reading Line..." << std::endl;
+	char buffer[256];
+	ssr::TimeSpec timeout(3, 0);
+	int ret = m_pSerialPort->ReadLineWithTimeout(buffer, 256, timeout, "\x0A\x0D"); // CRLF Throw away
+	ret = m_pSerialPort->ReadLineWithTimeout(buffer, 256, timeout, "\x0A\x0D"); // CRLF
+	if(ret > 0) {
+		std::cout << "[RTC.IMURazor9DOF] Read Line is :" << buffer << std::endl;
+	}
+	while (true) { // Timeout
+		if(buffer[0] == '$') {
+			break;
+		}
+		std::cout << "[RTC.IMURazor9DOF] Invalid Raw Data Line. Sending Ctrl+Z (0x1A) Data." << std::endl;
+		char buf = 0x1A; // Ctrl + Z
+		m_pSerialPort->Write(&buf, 1);
+		std::cout << "[RTC.IMURazor9DOF] Sleeping 1sec and Flush Received Data" << std::endl;
+		ssr::Thread::Sleep(1000);
+		m_pSerialPort->FlushRxBuffer();
+
+		std::cout << "[RTC.IMURazor9DOF] Reading Line..." << std::endl;
+		m_pSerialPort->ReadLineWithTimeout(buffer, 256, timeout, "\x0A\x0D");
+		ret = m_pSerialPort->ReadLineWithTimeout(buffer, 256, timeout, "\x0A\x0D"); // CRLF
+		if(ret < 0) {
+			std::cout << "[RTC.IMURazor9DOF] Timeout" << std::endl;
+			return RTC::RTC_ERROR;
+		}
+	}
+	std::cout << "[RTC.IMURazor9DOF] Successfully Initialized." << std::endl;
+	m_timer.tick();
+
+	m_pThread = new WorkerThread(this);
+	m_pThread->Start();
+	return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t IMURazor9DOF::onDeactivated(RTC::UniqueId ec_id)
+{
+	m_pThread->stop();
+	delete m_pSerialPort;
+  return RTC::RTC_OK;
+}
+
+
+
+
+RTC::ReturnCode_t IMURazor9DOF::onExecute(RTC::UniqueId ec_id)
+{
+	return RTC::RTC_OK;
+}
+
+RTC::ReturnCode_t IMURazor9DOF::onProcess()
+{
+	ssr::TimeSpec currentTime;
+	m_timer.tack(&currentTime);
+	ssr::TimeSpec timeout(3, 0);
+	char buffer[256];
+	//int ret = m_pSerialPort->ReadLineWithTimeout(buffer, 256, timeout, "\x0A\x0D"); // CRLF
+	int ret = m_pSerialPort->ReadLine(buffer, 256, "\x0A\x0D"); // CRLF
+	if(ret < 0) {
+		std::cout << "[RTC.IMURazor9DOF] In OnExecute: Timeout" << std::endl;
+		return RTC::RTC_ERROR;
+	}
+
+	// Error Check
+	if(buffer[0] != '$' ||  buffer[strlen(buffer)-1] != '#') {
+		std::cout << "[RTC.IMURazor9DOF] In OnExecute: Invalid Header" << std::endl;
+		return RTC::RTC_OK;
+	} 
+	/**
+	int num_comma = 0;
+	for(int i = 0;i < strlen(buffer);i++) {
+		if(buffer[i] == ',') {
+			num_comma++;
+		}
+	}
+	if(num_comma != 8) {
+		std::cout << "Invalid Data (Invalid Number of Element" << std::endl;
+		return RTC::RTC_OK;
+	}
+	*/
+
+
+	int rax, ray, raz, rwx, rwy, rwz, rmx, rmy, rmz;
+	int res = sscanf(buffer, "$%d, %d, %d, %d, %d, %d, %d, %d, %d#", &rax, &ray, &raz, &rwx, &rwy, &rwz, &rmx, &rmy, &rmz);
+	if(res == EOF) {
+		std::cout << "[RTC.IMURazor9DOF] In OnExecute: Invalid Format" << std::endl;
+		return RTC::RTC_OK;
+	}
+
+	double ax, ay, az, wx, wy, wz, mx, my, mz;
+
+#define G 9.80665
+#define PI 3.1415926
+
+	// 2G mode
+	m_accel.data.ax = rax / 255.0 * G; // m/sec^2
+	m_accel.data.ay = ray / 255.0 * G;
+	m_accel.data.az = raz / 255.0 * G;
+	::setTimestamp<RTC::TimedAcceleration3D>(m_accel);
+	m_accelOut.write();
+
+	m_angularVel.data.avx = rwx / ((double)(0x7FFF)) * 2000.0 / 180 * PI; // rad/sec
+	m_angularVel.data.avy = rwy / ((double)(0x7FFF)) * 2000.0 / 180 * PI;
+	m_angularVel.data.avz = rwz / ((double)(0x7FFF)) * 2000.0 / 180 * PI;
+	::setTimestamp<RTC::TimedAngularVelocity3D>(m_angularVel);
+	m_angularVelOut.write();
+
+	
+	if(m_debug) {
+		static int counter;
+		static double rad;
+		static double old_wz;
+		double dt = (currentTime.getUsec() - m_timestamp.getUsec()) / (1000.0*1000.0);
+		rad += (wz + old_wz) * dt /2;
+
+		if((counter++) % 10 == 0) 
+			std::cout << "rad: " << rad << std::endl;
+		old_wz = wz;
+
+		m_timestamp = currentTime;
+	}
+	return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t IMURazor9DOF::onAborting(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t IMURazor9DOF::onError(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t IMURazor9DOF::onReset(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t IMURazor9DOF::onStateUpdate(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t IMURazor9DOF::onRateChanged(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+
+extern "C"
+{
+ 
+  void IMURazor9DOFInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(imurazor9dof_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<IMURazor9DOF>,
+                             RTC::Delete<IMURazor9DOF>);
+  }
+  
+};
+
+
