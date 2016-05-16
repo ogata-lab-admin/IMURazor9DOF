b5f7a8f58a9d8762537e8e738e7ec5e08919a5b5
Yuki Suga <ysuga@ysuga.net>
2016-05-16 17:15:25 +0900

    update for magnet data

--- src/IMURazor9DOF.cpp
+++ src/IMURazor9DOF.cpp
@@ -28,7 +28,7 @@ static const char* imurazor9dof_spec[] =
     "language",          "C++",
     "lang_type",         "compile",
     // Configuration variables
-    "conf.default.debug", "0",
+    "conf.default.debug", "1",
     "conf.default.port", "\\\\.\\COM20",
     "conf.default.baudrate", "57600",
     // Widget
@@ -194,33 +194,33 @@ RTC::ReturnCode_t IMURazor9DOF::onProcess()
 	char buffer[256];
 	//int ret = m_pSerialPort->ReadLineWithTimeout(buffer, 256, timeout, "\x0A\x0D"); // CRLF
 	int ret = m_pSerialPort->ReadLine(buffer, 256, "\x0A\x0D"); // CRLF
-	if(ret < 0) {
+	if (ret < 0) {
 		std::cout << "[RTC.IMURazor9DOF] In OnExecute: Timeout" << std::endl;
 		return RTC::RTC_ERROR;
 	}
 
 	// Error Check
-	if(buffer[0] != '$' ||  buffer[strlen(buffer)-1] != '#') {
+	if (buffer[0] != '$' || buffer[strlen(buffer) - 1] != '#') {
 		std::cout << "[RTC.IMURazor9DOF] In OnExecute: Invalid Header" << std::endl;
 		return RTC::RTC_OK;
-	} 
+	}
 	/**
 	int num_comma = 0;
 	for(int i = 0;i < strlen(buffer);i++) {
-		if(buffer[i] == ',') {
-			num_comma++;
-		}
+	if(buffer[i] == ',') {
+	num_comma++;
+	}
 	}
 	if(num_comma != 8) {
-		std::cout << "Invalid Data (Invalid Number of Element" << std::endl;
-		return RTC::RTC_OK;
+	std::cout << "Invalid Data (Invalid Number of Element" << std::endl;
+	return RTC::RTC_OK;
 	}
 	*/
 
 
 	int rax, ray, raz, rwx, rwy, rwz, rmx, rmy, rmz;
 	int res = sscanf(buffer, "$%d, %d, %d, %d, %d, %d, %d, %d, %d#", &rax, &ray, &raz, &rwx, &rwy, &rwz, &rmx, &rmy, &rmz);
-	if(res == EOF) {
+	if (res == EOF) {
 		std::cout << "[RTC.IMURazor9DOF] In OnExecute: Invalid Format" << std::endl;
 		return RTC::RTC_OK;
 	}
@@ -231,28 +231,55 @@ RTC::ReturnCode_t IMURazor9DOF::onProcess()
 #define PI 3.1415926
 
 	// 2G mode
-	m_accel.data.ax = rax / 255.0 * G; // m/sec^2
-	m_accel.data.ay = ray / 255.0 * G;
-	m_accel.data.az = raz / 255.0 * G;
+	m_accel.data.ax = ax = rax / 255.0 * G; // m/sec^2
+	m_accel.data.ay = ay = ray / 255.0 * G;
+	m_accel.data.az = az = raz / 255.0 * G;
 	::setTimestamp<RTC::TimedAcceleration3D>(m_accel);
 	m_accelOut.write();
 
-	m_angularVel.data.avx = rwx / ((double)(0x7FFF)) * 2000.0 / 180 * PI; // rad/sec
-	m_angularVel.data.avy = rwy / ((double)(0x7FFF)) * 2000.0 / 180 * PI;
-	m_angularVel.data.avz = rwz / ((double)(0x7FFF)) * 2000.0 / 180 * PI;
+	m_angularVel.data.avx = wx = rwx / ((double)(0x7FFF)) * 2000.0 / 180 * PI; // rad/sec
+	m_angularVel.data.avy = wy = rwy / ((double)(0x7FFF)) * 2000.0 / 180 * PI;
+	m_angularVel.data.avz = wz = rwz / ((double)(0x7FFF)) * 2000.0 / 180 * PI;
 	::setTimestamp<RTC::TimedAngularVelocity3D>(m_angularVel);
 	m_angularVelOut.write();
 
-	
+	mx = 2.56 * rmx;
+	my = 2.56 * rmy;
+	mz = 2.56 * rmz;
+
+	this->m_magnet.data.length(4);
+	double a = sqrt(ax*ax + ay*ay + az*az);
+	double m = sqrt(mx*mx + my*my + mz*mz);
+
+	double hx = my*az - mz*ay;
+	double hy = mz*ax - mx*az;
+	double hz = mx*ay - my*ax;
+	double h = sqrt(hx*hx + hy*hy + hz*hz);
+
+	double rx = ay*hz - az*hy;
+	double ry = az*hx - ax*hz;
+	double rz = ax*hy - ay*hx;
+	double r = sqrt(rx*rx + ry*ry + rz*rz);
+	double heading = -atan2(hx / h, rx / r);
+
+	m_magnet.data[0] = heading;
+	m_magnet.data[1] = mx;
+	m_magnet.data[2] = my;
+	m_magnet.data[3] = mz;
+	setTimestamp<RTC::TimedDoubleSeq>(m_magnet);
+	m_magnetOut.write();
+
+
 	if(m_debug) {
 		static int counter;
 		static double rad;
 		static double old_wz;
+		wz = m_angularVel.data.avz;
 		double dt = (currentTime.getUsec() - m_timestamp.getUsec()) / (1000.0*1000.0);
 		rad += (wz + old_wz) * dt /2;
 
 		if((counter++) % 10 == 0) 
-			std::cout << "rad: " << rad << std::endl;
+			std::cout << "rad: " << rad << ", heading: " << heading << std::endl;
 		old_wz = wz;
 
 		m_timestamp = currentTime;
